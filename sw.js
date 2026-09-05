e89640623aca607f4444e3a399b91a91aef1d2bb
anshm4257-creator <anshm4257@gmail.com>
2026-09-05 20:47:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "studysprint-v1";
+const CACHE_NAME = "prepvio-v1";
 
-const FILES = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json"
@@ -8,13 +8,26 @@ const FILES = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
